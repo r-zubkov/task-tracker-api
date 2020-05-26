287a53446ae3fa6c89324f934acb048fabc8d1e6
insane-ray <insane4ray@gmail.com>
2020-05-26 05:18:20 +0300

    task module

--- src/modules/task-comment/task-comment.entity.ts
+++ src/modules/task-comment/task-comment.entity.ts
@@ -11,11 +11,17 @@ export class TaskComment {
   @Column({length: 1000, nullable: false})
   text: string;
 
+  @Column({name: 'change_reason', length: 150, nullable: true})
+  changeReason: string;
+
   @ManyToOne(() => User, user => user.taskAuthor, {nullable: false})
   author: User;
 
   @ManyToOne(() => Task, task => task.taskComments, {nullable: false})
   task: Task;
+
+  @Column('datetime', {name: 'updated_at', nullable: true})
+  updatedAt: Date;
 
   @Column('datetime', {name: 'created_at', nullable: false})
   createdAt: Date;
