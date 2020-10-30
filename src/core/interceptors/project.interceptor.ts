e8173a7801bca7bba3fcd4435447efd7b2939be5
InsaneRay <r.zubkov.et@gmail.com>
2020-10-30 17:48:15 +0300

    feat: add id validation to project interceptor

--- src/core/interceptors/project.interceptor.ts
+++ src/core/interceptors/project.interceptor.ts
@@ -1,7 +1,15 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+  HttpStatus,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { from, Observable } from 'rxjs';
 import { ProjectService } from '../../modules/project/project.service';
-import { switchMap, tap } from 'rxjs/operators';
+import { catchError, switchMap, tap } from 'rxjs/operators';
 
 @Injectable()
 export class ProjectInterceptor implements NestInterceptor {
@@ -9,24 +17,33 @@ export class ProjectInterceptor implements NestInterceptor {
   constructor(private projectService: ProjectService) {}
 
   /**
-   * Find and add project to request
+   * check id validity
+   * then and add project to request
    * or return 404 if not
    */
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
     const projectId = request.params.projectId;
-    const projectEntity = this.projectService.findEntity(projectId, user);
 
-    return from(projectEntity)
+    return from((new ParseUUIDPipe()).transform(projectId, { type: 'param' }))
       .pipe(
-        tap(entity => {
-          if (!entity) {
-            throw new HttpException("Project not found", HttpStatus.NOT_FOUND)
-          }
-          request.project = entity;
+        catchError(() => {
+          throw new HttpException("Invalid ID", HttpStatus.BAD_REQUEST);
         }),
-        switchMap(() => next.handle())
+        switchMap(() => {
+          const projectEntity = this.projectService.findEntity(projectId, user);
+          return from(projectEntity)
+            .pipe(
+              tap(entity => {
+                if (!entity) {
+                  throw new HttpException("Project not found", HttpStatus.NOT_FOUND)
+                }
+                request.project = entity;
+              }),
+              switchMap(() => next.handle())
+            )
+        })
       )
   }
 }
